8ec1284f77944769cb0df72c231aa429c91ff658
Jack Ashman <91268292+Jack-Ashman@users.noreply.github.com>
2023-10-27 10:53:07 +0100

    Updated scrape.sh

--- scripts/scrape.sh
+++ scripts/scrape.sh
@@ -1,46 +1,17 @@
 #! /bin/bash
 
-# KPMs
-#
-#* RAM_USED
-#* RAM_FREE
-#* SWAP_USED
-#* SWAP_FREE
-#
-# DISK_USED
-# DISK_FREE
-#
-#* CPU_CORES
-#* CPU_THREADS
-#
-#* LOAD_1
-#* LOAD_5
-#* LOAD_15
-#
-#* UPTIME
-# TASKS
-#
-#* CONNECTED
-#* HOSTNAME
-#* IP_ADDRESS
-#* OS
-#* TIMESTAMP
-
-
-
-# top - 12:33:34 up 170 days,  4:24,  2 users,  load average: 0.18, 0.25, 0.28
-# Tasks: 258 total,   1 running, 256 sleeping,   0 stopped,   1 zombie
-# %Cpu(s):  1.5 us,  2.9 sy,  0.0 ni, 95.6 id,  0.0 wa,  0.0 hi,  0.0 si,  0.0 st
-# KiB Mem :  8148192 total,   921368 free,  2407612 used,  4819212 buff/cache
-# KiB Swap:  5242876 total,  4984316 free,   258560 used.  5130848 avail Mem
-
-
-# COMMANDS
-
-# Basic Calculator
-
-# bc [ -hlwsqv ] [long-options] [ file ... ]
-
+# Ansi color code variables
+red="\e[0;91m"
+blue="\e[0;94m"
+expand_bg="\e[K"
+blue_bg="\e[0;104m${expand_bg}"
+red_bg="\e[0;101m${expand_bg}"
+green_bg="\e[0;102m${expand_bg}"
+green="\e[0;92m"
+white="\e[0;97m"
+bold="\e[1m"
+uline="\e[4m"
+reset="\e[0m"
 
 
 # CONNECTED
@@ -83,7 +54,6 @@ CPU_CORES=$(lscpu | grep "CPU(s)" | awk 'NR==1 {print $2}')
 CPU_THREADS=$(nproc) # Threads per Core X Cores per Socket X Sockets
 
 
-
 # CPU_USE & CPU_IDLE
 
 CPU_IDLE=$(top -bn1 -E k | grep "Cpu(s)" | awk -F ', ' '{print $4*1}')
@@ -94,8 +64,6 @@ CPU_USE=$(echo "100 - $CPU_IDLE" | bc)
 
 DISK_USED=$(df -BKB / | awk 'NR==2 {print $3*1}')
 DISK_FREE=$(df -BKB / | awk 'NR==2 {print $4*1}')
-
-# USE IWJS TO SEE VOLUMES (VDA/SDA)
 
 
 # #################################################################################################################### #
@@ -128,38 +96,37 @@ OUTPUT="""{
     \"SWAP_USED\": $SWAP_USED,
 
     \"DISK_FREE\": $DISK_FREE,
-    \"DISK_USED\": $DISK_USED,
+    \"DISK_USED\": $DISK_USED
 }"""
 
-echo """{
-    \"CONNECTED\": $CONNECTED,
+# echo -e """{
+#     \"CONNECTED\": ${green}$CONNECTED${reset},
 
-    \"HOSTNAME\": \"$HOSTNAME\",
-    \"IP_ADDRESS\": \"$IP_ADDRESS\",
-    \"OS\": \"$OS\",
+#     \"HOSTNAME\": \"$HOSTNAME\",
+#     \"IP_ADDRESS\": \"$IP_ADDRESS\",
+#     \"OS\": \"$OS\",
 
-    \"TIMESTAMP\": $TIMESTAMP,
-    \"UPTIME\": $UPTIME,
+#     \"TIMESTAMP\": $TIMESTAMP,
+#     \"UPTIME\": $UPTIME,
 
-    \"CPU_CORES\": $CPU_CORES,
-    \"CPU_THREADS\": $CPU_THREADS,
+#     \"CPU_CORES\": $CPU_CORES,
+#     \"CPU_THREADS\": $CPU_THREADS,
 
-    \"CPU_USE\": $CPU_USE,
-    \"CPU_IDLE\": $CPU_IDLE,
+#     \"CPU_USE\": $CPU_USE,
+#     \"CPU_IDLE\": $CPU_IDLE,
 
-    \"LOAD_1\": $LOAD_1,
-    \"LOAD_5\": $LOAD_5,
-    \"LOAD_15\": $LOAD_15,
+#     \"LOAD_1\": $LOAD_1,
+#     \"LOAD_5\": $LOAD_5,
+#     \"LOAD_15\": $LOAD_15,
 
-    \"RAM_FREE\": $RAM_FREE,
-    \"RAM_USED\": $RAM_USED,
+#     \"RAM_FREE\": $RAM_FREE,
+#     \"RAM_USED\": $RAM_USED,
 
-    \"SWAP_FREE\": $SWAP_FREE,
-    \"SWAP_USED\": $SWAP_USED,
+#     \"SWAP_FREE\": $SWAP_FREE,
+#     \"SWAP_USED\": $SWAP_USED,
 
-    \"DISK_FREE\": $DISK_FREE,
-    \"DISK_USED\": $DISK_USED,
-}"""
+#     \"DISK_FREE\": $DISK_FREE,
+#     \"DISK_USED\": $DISK_USED
+# }""";
 
-
-wget --method=post -O- -q --body-data="$OUTPUT" --header='Content-Type:application/json' http://dev.portal.senses.co.uk/api/servers/scrape
+wget --method=post -O- --body-data="$OUTPUT" --header=Content-Type:application/json http://dev.portal.senses.co.uk/api/servers/scrape
