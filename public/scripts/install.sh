a9f432dcff2f06779b1a37c7a00525cbe0113ed2
Jack Ashman <91268292+Jack-Ashman@users.noreply.github.com>
2023-10-30 15:17:25 +0000

    Added basic install script

--- public/scripts/install.sh
+++ public/scripts/install.sh
@@ -1,3 +1,9 @@
 #! /bin/bash
 
+
+# Download the scraper script
 wget -q http://dev.portal.senses.co.uk/scripts/scrape.sh
+
+
+# Add the job to the crontab
+crontab -l | { cat; echo -e "\n# Run Senses Server Scraper every minute (With a 30 second offset)\n* * * * * ( sleep 30; bash /home/forge/scrape.sh )"; } | crontab -
