f39dc2aa93f5e392016413855ad269f61cba262b
Jack Ashman <91268292+Jack-Ashman@users.noreply.github.com>
2023-11-01 11:53:05 +0000

    Removed Request Rules

--- app/Http/Requests/ServerMetrics/CreateServerMetricRequest.php
+++ app/Http/Requests/ServerMetrics/CreateServerMetricRequest.php
@@ -12,46 +12,46 @@ class CreateServerMetricRequest extends FormRequest
 {
     public function authorize(): bool
     {
+		return true;
+
 		if (getCurrentUser()?->can('create-server-metric')) {
 			return true;
 		}
-
-        return false;
     }
 
     public function rules()
     {
         $rules = [
-			'server_id' => 'required|integer|exists:servers,id',
-			'company_id' => 'required|integer|exists:companies,id',
-			'timestamp' => 'nullable|integer|min:0',
-			'uptime' => 'nullable|integer|min:0',
-			'cpu_use' => 'nullable',
-			'cpu_us' => 'nullable',
-			'cpu_sy' => 'nullable',
-			'cpu_ni' => 'nullable',
-			'cpu_id' => 'nullable',
-			'cpu_wa' => 'nullable',
-			'cpu_hi' => 'nullable',
-			'cpu_si' => 'nullable',
-			'cpu_st' => 'nullable',
-			'load_1' => 'nullable',
-			'load_5' => 'nullable',
-			'load_15' => 'nullable',
-			'ram_total' => 'nullable',
-			'ram_free' => 'nullable',
-			'ram_buffer' => 'nullable',
-			'ram_cache' => 'nullable',
-			'ram_used' => 'nullable',
-			'swap_total' => 'nullable',
-			'swap_free' => 'nullable',
-			'swap_used' => 'nullable',
-			'swap_cache' => 'nullable',
-			'disk_total' => 'nullable|integer|min:0',
-			'disk_free' => 'nullable|integer|min:0',
-			'disk_used' => 'nullable|integer|min:0',
-			'disk_read' => 'nullable|integer|min:0',
-			'disk_write' => 'nullable|integer|min:0',
+			// 'server_id' => 'required|integer|exists:servers,id',
+			// 'company_id' => 'required|integer|exists:companies,id',
+			// 'timestamp' => 'nullable|integer|min:0',
+			// 'uptime' => 'nullable|integer|min:0',
+			// 'cpu_use' => 'nullable',
+			// 'cpu_us' => 'nullable',
+			// 'cpu_sy' => 'nullable',
+			// 'cpu_ni' => 'nullable',
+			// 'cpu_id' => 'nullable',
+			// 'cpu_wa' => 'nullable',
+			// 'cpu_hi' => 'nullable',
+			// 'cpu_si' => 'nullable',
+			// 'cpu_st' => 'nullable',
+			// 'load_1' => 'nullable',
+			// 'load_5' => 'nullable',
+			// 'load_15' => 'nullable',
+			// 'ram_total' => 'nullable',
+			// 'ram_free' => 'nullable',
+			// 'ram_buffer' => 'nullable',
+			// 'ram_cache' => 'nullable',
+			// 'ram_used' => 'nullable',
+			// 'swap_total' => 'nullable',
+			// 'swap_free' => 'nullable',
+			// 'swap_used' => 'nullable',
+			// 'swap_cache' => 'nullable',
+			// 'disk_total' => 'nullable|integer|min:0',
+			// 'disk_free' => 'nullable|integer|min:0',
+			// 'disk_used' => 'nullable|integer|min:0',
+			// 'disk_read' => 'nullable|integer|min:0',
+			// 'disk_write' => 'nullable|integer|min:0',
         ];
 
         return $rules;
