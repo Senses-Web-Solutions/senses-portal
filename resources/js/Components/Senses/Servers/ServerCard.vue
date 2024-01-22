7c91a75191e76f3e61c3dbbc1eb0577df55e6285
Jack Ashman <91268292+Jack-Ashman@users.noreply.github.com>
2024-01-22 10:47:13 +0000

    Added an ignore danger feature

--- resources/js/Components/Senses/Servers/ServerCard.vue
+++ resources/js/Components/Senses/Servers/ServerCard.vue
@@ -22,7 +22,7 @@
 
                 <!-- Middle Bit -->
                 <div class="absolute left-[70px] right-[70px] top-[40px]">
-                    <svg v-if="icon == 'danger'" xmlns="http://www.w3.org/2000/svg" class="text-red-400 animate-pulse" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                    <svg v-if="icon == 'danger' && !dangerIgnored" @click="dangerIgnored = true; updateStatus();" xmlns="http://www.w3.org/2000/svg" class="text-red-400 animate-pulse" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
                     </svg>
 
@@ -192,6 +192,8 @@ export default {
             circle15: null,
 
             icon: 0,
+
+            dangerIgnored: false,
         };
     },
 
